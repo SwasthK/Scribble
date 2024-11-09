4c02ef8424cd5cd367d3db60a7f5a15f89ae32b8
SwasthK <220981@sdmcujire.in>
2024-11-09 20:50:05 +0530

    Update Server

--- server/src/Middleware/Auth.ts
+++ server/src/Middleware/Auth.ts
@@ -4,11 +4,11 @@ import { verifyTokens } from "../utils/jwt";
 
 export async function authMiddleware(c: Context, next: Next) {
     try {
-
+        console.log("authMiddleware");
         const accessToken = c.req.header('accessToken')?.split(' ')[1]
 
         if (!accessToken) return apiError(c, 401, "Failed to Authorize User 1");
-        
+
         const decodeAccessToken = await verifyTokens(c, accessToken, c.env.ACCESS_TOKEN_SECRET);
 
         if (!decodeAccessToken) {
@@ -18,7 +18,8 @@ export async function authMiddleware(c: Context, next: Next) {
         c.set('id', decodeAccessToken.id);
 
         await next();
-    } catch (error) {
+    } catch (error: any) {
+        console.log(error.message);
         return c.json({ message: "Something went wrong" }, 400)
     }
 }
