4340ee9c3421c846a29ff04ae790bbe84bb28fd6
SwasthK <220981@sdmcujire.in>
2024-11-22 18:35:22 +0530

    Add Controller for Get Comment

--- server/src/Controllers/Comments/get.Comment.Post.ts
+++ server/src/Controllers/Comments/get.Comment.Post.ts
@@ -50,3 +50,36 @@ export async function getComments(c: Context) {
     }
 
 }
+
+export async function getCommentsWithoutReply(c: Context) {
+    try {
+        const postId = c.req.param("postId");
+        if (!postId) { return apiError(c, 400, "Post ID is required"); }
+
+        const prisma: any = await c.get("prisma");
+
+        const comments = await prisma.comment.findMany({
+            where: { postId, parentId: null },
+            select: {
+                content: true,
+                createdAt: true,
+                author: {
+                    select: {
+                        username: true,
+                        avatarUrl: true
+                    }
+                }
+            },
+
+            orderBy: { createdAt: "desc" }
+        })
+        console.log("comments", comments);
+
+        return apiResponse(c, 200, { comments }, "Comments retrieved successfully");
+    }
+
+    catch (error: any) {
+        console.log("Error in getCommentsWithoutReply: ", error);
+        return apiError(c, 500, "Internal server error", { code: "CE" });
+    }
+}
