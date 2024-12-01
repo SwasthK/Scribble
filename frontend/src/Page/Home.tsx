f782b7f8d211c9c017fff1815c7af262f26c110e
SwasthK <220981@sdmcujire.in>
2024-12-01 23:12:48 +0530

    Update Home Pagew

--- frontend/src/Page/Home.tsx
+++ frontend/src/Page/Home.tsx
@@ -1,6 +1,11 @@
 // import React from 'react';
 // import { AppBar } from "../components/AppBar/AppBar";
+import { Link } from "react-router-dom";
 import { LandingBg } from "../components/Home/LandingBg";
+import { GitHubIcon } from "../assets/svg/GitHubIcon";
+import { TwitterIcon } from "../assets/svg/TwitterIcon";
+import { InstagramIcon } from "../assets/svg/InstagramIcon";
+import { Mail } from "lucide-react";
 // import { Outlet } from "react-router-dom";
 
 const Home = () => {
@@ -20,7 +25,7 @@ const Home = () => {
       {/* Scrollable Content */}
       <div className="relative z-10 h-full w-full overflow-y-scroll">
         <div className="w-[90%] mx-auto text-white ">
-          <div className="h-screen flex justify-center items-center  flex-col gap-16">
+          <div className="h-screen flex justify-center items-center  flex-col gap-10  relative">
             <div className="relative">
               <h1 className="mb-2 text-7xl sm:text-9xl font-scribble2 sm:mb-9">
                 Scribble .
@@ -29,7 +34,11 @@ const Home = () => {
                 Unleash Your Voice, Share Your Story !
               </h3>
             </div>
-            <div className=" flex gap-5 sm:gap-12 w-full justify-center">
+            <h1 className="  text-[#8C969B]  text-center">
+              Write, connect, and inspire with our modern blogging platform
+            </h1>
+
+            <div className="flex gap-5 sm:gap-12 w-full justify-center mt-4">
               <button className="px-6 py-2 sm:px-10 sm:py-5 rounded-full border text-black bg-white font-extralight">
                 Register
               </button>
@@ -42,29 +51,66 @@ const Home = () => {
             </div>
           </div>
 
-          {/* First Section */}
-          <div className="flex items-center gap-8 flex-col lg:flex-row lg:gap-24 py-8 justify-center">
-            <img
-              src="/Block-animation.gif"
-              className="border border-alphaborder w-[30rem] object-contain rounded-lg"
-            ></img>
-            <div className="max-w-[30rem] flex flex-col gap-10">
-              <h1 className="text-3xl md:text-5xl font-semibold">
-                Rich Text Editing Made Easy
-              </h1>
-              <p className="md:text-lg text-neutral-400">
-              Craft beautiful content effortlessly with our intuitive BlockNote editor.
-              </p>
+          <div
+            className="rounded-xl sm:py-16 py-4 flex flex-col gap-10 border lg:px-16"
+            style={{
+              background: "linear-gradient(to bottom, #051E2E, #000)",
+            }}
+          >
+            <div className="flex items-center gap-12 flex-col lg:flex-row lg:gap-24 py-8 justify-center">
+              <img
+                src="/Block-animation.gif"
+                className="border border-alphaborder w-[25rem] sm:w-[30rem] object-contain rounded-lg"
+              ></img>
+              <div className="max-w-[30rem] flex flex-col gap-4 sm:gap-10 font-scribble2 -tracking-wide">
+                <h1 className="text-3xl md:text-5xl font-semibold">
+                  Rich Text Editing Made Easy .
+                </h1>
+                <p className="md:text-lg text-neutral-400">
+                  Craft beautiful content effortlessly with our intuitive <br />
+                  <Link
+                    to={"https://www.blocknotejs.org/"}
+                    target="_blank"
+                    className="text-[#EE9521] hover:underline"
+                  >
+                    BlockNote editor .
+                  </Link>
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-12 flex-col lg:flex-row-reverse lg:gap-24 py-8 justify-center">
+              <img
+                src="/Block-animation.gif"
+                className="border border-alphaborder w-[25rem] sm:w-[30rem]  object-contain rounded-lg"
+              ></img>
+              <div className="max-w-[30rem] flex flex-col gap-4 sm:gap-10 font-scribble2 -tracking-wide">
+                <h1 className="text-3xl md:text-5xl font-semibold ">
+                  Advanced Search .
+                </h1>
+                <p className="md:text-lg text-neutral-400">
+                  Quickly discover posts, categories, and users with <br /> our
+                  custom{" "}
+                  <Link
+                    to={"https://www.blocknotejs.org/"}
+                    target="_blank"
+                    className="text-[#EE9521] hover:underline"
+                  >
+                    ShadCN{" "}
+                  </Link>
+                  - powered advanced search .
+                </p>
+              </div>
             </div>
           </div>
 
           {/* Second Section */}
-          <div className="w-full bg-[#0C0C0C] sm:px-16 sm:py-16 p-16 rounded-xl">
-            <div className=" flex gap-4 flex-col w-full">
-              <p className="font-semibold w-[100%]  md:w-[30rem]">
+          <div className="w-full bg-[#0C0C0C] sm:py-16 sm:pt-20 pb-12 pt-20 flex flex-col gap-10 lg:px-16 sm:px-16    rounded-tr-xl rounded-tl-xl font-scribble2">
+            <div className=" flex gap-4 flex-col w-full  px-8 sm:px-0">
+              <p className="font-semibold w-[100%] md:w-[30rem] text-sm text-[#95959D]">
                 Runway | Tools for human imagination.
               </p>
-              <p className="font-scribble1 text-[#585F69  ] font-[100] sm:text-4xl text-2xl w-[100%]  md:w-[30rem] lg:w-[35rem]">
+              <p className="font-scribble2 font-[700] text-[#585F69  ] font-[100] sm:text-4xl text-2xl w-[100%]  md:w-[30rem] lg:w-[35rem]">
                 We are pioneering general-purpose multimodal simulators of the
                 world.
               </p>
@@ -73,29 +119,130 @@ const Home = () => {
               </button>
             </div>
 
-            <div className=" grid lg:grid-cols-3 gap-12 sm:gap-14 md:gap-20 lg:gap-10 grid-cols-1 place-items-center mt-8 ">
-              {Array.from({ length: 3 }).map((_, idx) => (
-                <div
-                  key={idx}
-                  className="flex gap-2 flex-col items-center  w-full max-w-96"
-                >
-                  <img
-                    src="https://cdn.wccftech.com/wp-content/uploads/2023/07/Cyberpunk-2077-Project-AI.jpg"
-                    alt=""
-                    className="xl:h-60 mb-4  w-full rounded-xl object-cover object-center"
-                  />
-                  <h1 className="font-semibold text-[1.1rem]  w-full  ">
-                    Lorem, ipsum dolor sit amet consectetur
-                  </h1>
-                  <h6 className="text-sm text-gray-400  w-full  ">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Qui, quis.
-                  </h6>
-                </div>
-              ))}
+            <div className=" grid lg:grid-cols-3 gap-12 sm:gap-14 md:gap-20 lg:gap-10 grid-cols-1 place-items-center sm:mt-16 mt-8">
+              <div className="flex gap-2 flex-col items-center  w-full max-w-96">
+                <img
+                  src="/Block-animation.gif"
+                  alt=""
+                  className="xl:h-60 mb-4  w-full rounded-md object-cover object-center"
+                />
+                <h1 className="font-semibold text-[1.1rem]  w-full  ">
+                  Draft, Archive, Publish
+                </h1>
+                <h6 className="text-sm text-gray-400  w-full  ">
+                  "Manage your posts your way — work in progress, organize
+                  archived ideas, or share with the world.
+                </h6>
+              </div>
+
+              <div className="flex gap-2 flex-col items-center  w-full max-w-96">
+                <img
+                  src="/Block-animation.gif"
+                  alt=""
+                  className="xl:h-60 mb-4  w-full rounded-md object-cover object-center"
+                />
+                <h1 className="font-semibold text-[1.1rem]  w-full  ">
+                  Engage & Connect
+                </h1>
+                <h6 className="text-sm text-gray-400  w-full  ">
+                  "Follow creators, comment on posts, and build meaningful
+                  connections."
+                </h6>
+              </div>
+
+              <div className="flex gap-2 flex-col items-center  w-full max-w-96">
+                <img
+                  src="/Block-animation.gif"
+                  alt=""
+                  className="xl:h-60 mb-4  w-full rounded-md object-cover object-center"
+                />
+                <h1 className="font-semibold text-[1.1rem]  w-full  ">
+                  Like, Save & Report
+                </h1>
+                <h6 className="text-sm text-gray-400  w-full  ">
+                  "Engage with posts you love, save them for later, or report
+                  inappropriate content."
+                </h6>
+              </div>
+            </div>
+          </div>
+
+          <div className="sm:px-16 sm:pt-32 px-8 pt-8 flex flex-col  justify-center items-center font-scribble2 rounded-xl">
+            <div className="text-center">
+              <h1 className="text-5xl font-bold">Personalized Profiles</h1>
+              <p className="text-[#666] mt-4">
+                Set your username, avatar, and social links to showcase your
+                unique identity.
+              </p>
+            </div>
+            <div className="grid sm:grid-cols-2 grid-cols-1 gap-x-16 mt-8 gap-y-10">
+              <img
+                src="/Block-animation.gif"
+                alt="Profile example 1"
+                className="w-60 h-60  rounded-md object-contain"
+              />
+              <img
+                src="/Block-animation.gif"
+                alt="Profile example 2"
+                className="w-60 h-60  rounded-md object-contain"
+              />
+            </div>
+          </div>
+
+          <div className="h-screen sm:px-8 sm:py-24 px-5 py-8  flex flex-col justify-center items-center font-scribble2">
+            <div className="sm:text-center pb-8">
+              <h1 className="text-6xl font-bold">
+                Ready to Start Your Blogging Journey?
+              </h1>
+              <p className="text-[#666] mt-4">
+                Join thousands of creators sharing their voices and building
+                communities.
+              </p>
+            </div>
+            <div className=" flex gap-5 sm:gap-12 w-full justify-center mt-4">
+              <Link to="/register"
+
+              className="px-6 py-2 sm:px-10 sm:py-5 rounded-full border text-black bg-white font-extralight">
+                Register
+              </Link>
+              {/* <div className="flex flex-col"> */}
+              <Link to="/register" className="font-scribble2 px-6 py-2 sm:px-10 sm:py-5 rounded-full border text-white bg-cdark-300">
+                Continue without Registering &#8594;
+              </Link >
+              {/* <h1></h1> */}
+              {/* </div> */}
             </div>
 
-            <div className="h-48 w-96 bg-[#252525]"></div>
+          </div>
+        </div>
+        <div className="w-screen font-scribble1 py-12 md:px-10 lg:px-36 text-sm flex justify-between items-center bg-[#09090B] flex-col md:flex-row gap-8 md:gap-0">
+          <p className="text-[#95959D]">
+            Built by{" "}
+            <Link
+              to={"https://www.blocknotejs.org/"}
+              target="_blank"
+              className="text-[#EE9521] hover:underline"
+            >
+              Swasthik{" "}
+            </Link>
+            . The source code is available on{" "}
+            <Link
+              to={"https://www.blocknotejs.org/"}
+              target="_blank"
+              className="text-[#EE9521] hover:underline"
+            >
+              Github{" "}
+            </Link>
+            {"."}
+          </p>
+          <div className="flex gap-10 items-center">
+            <p className="text-[#95959D]">Socials</p>
+            <div className="flex gap-7 items-center">
+              <GitHubIcon size={20} />
+              <TwitterIcon size={18} />
+              <InstagramIcon size={20} />
+              <Mail />
+            </div>
           </div>
         </div>
       </div>
@@ -130,3 +277,11 @@ export default Home;
 //     <p>&copy; 2024 My Blog. All rights reserved.</p>
 //   </footer> */}
 // // </div>
+
+const LandingButton = () => {
+  return (
+    <button className="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border-2 border-red-700 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full hover:text-white before:-right-full before:hover:right-0 before:rounded-full before:bg-[#A12347] before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 inline-flex items-center justify-center px-4 py-3 text-sm font-semibold text-black bg-white border border-gray-200 rounded-lg shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
+      Button
+    </button>
+  );
+};
