6e76cfacd536c8cd0d47fd1368fc3c203113e5f0
SwasthK <220981@sdmcujire.in>
2024-12-02 23:49:00 +0530

    Update NextIcon

--- frontend/src/assets/svg/Next.tsx
+++ frontend/src/assets/svg/Next.tsx
@@ -1,13 +1,16 @@
-export const Next = () => {
+import { memo } from "react";
+import { SvgIconType } from "../../Types/type";
+
+export const Next = memo(({ size }: SvgIconType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="30px"
+      height={`${size || 30}px`}
       viewBox="0 -960 960 960"
-      width="30px"
+      width={`${size || 30}px`}
       fill="#e8eaed"
     >
       <path d="m320.23-107.69-42.54-42.54L607.46-480 277.69-809.77l42.54-42.54L692.54-480 320.23-107.69Z" />
     </svg>
   );
-};
+});
