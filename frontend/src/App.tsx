e19ddde4108af842143c1fd438f99949bf3ff113
SwasthK <220981@sdmcujire.in>
2024-12-03 16:14:58 +0530

    Update App Page Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,215 +1,112 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { ProtectedRoute, UnProtectedRoute } from "./utils/Route.Protect";
+import { lazy, useEffect, useState } from "react";
 import { useRecoilState, useSetRecoilState } from "recoil";
-
-import Home from "./Page/Home";
-import { NotFound } from "./Page/NotFound";
-import { Signup } from "./Page/Signup";
-import { BlogContent } from "./Page/BlogContent";
-import { Publish } from "./Page/Publish";
-import { Loader } from "./Skeleton/Loader";
-import { Profile } from "./Page/Profile";
 import { refreshAccessToken } from "./utils/authHandler";
 import { authAtom, useAuthAtomResetValue } from "./atoms/auth.atoms";
-import { VideoPlayer } from "./components/Auth/VideoPlayer";
-import { Signin } from "./Page/Signin";
-import { Blogs } from "./Page/Blogs";
-import NoveEditor from "./Page/NoveEditor";
-import { NovelPreview } from "./Page/NovelPreview";
-import { HandlePost } from "./Page/HandlePost";
-import { Draft } from "./Page/Draft";
-import { Save } from "./Page/Save";
-import { Archived } from "./Page/Archived";
 import { followAtom } from "./atoms/followAtom";
 import { allUsersNames, postsAtom } from "./atoms";
-import { Followers } from "./Page/Followers";
-import { ProfileView } from "./Page/ProfileView";
-import { Topic } from "./Page/Topic";
 import axios from "axios";
+import { Loader } from "./Skeleton/Loader";
+import SuspendedComponent from "./Layout/SuspensedComponent";
+
+// Layout Imports
+import { LayoutProtected } from "./Layout/LayoutProtected";
+import { LayoutUnProtected } from "./Layout/LayoutUnProtected";
+
+// Public Layout Imports
+const Home = lazy(() => import("./Page/Home"));
+const Signup = lazy(() => import("./Page/Signup"));
+const Signin = lazy(() => import("./Page/Signin"));
+
+// Private Layout Imports
+const Blogs = lazy(() => import("./Page/Blogs"));
+const Profile = lazy(() => import("./Page/Profile"));
+const BlogContent = lazy(() => import("./Page/BlogContent"));
+const Draft = lazy(() => import("./Page/Draft"));
+const HandlePost = lazy(() => import("./Page/HandlePost"));
+const Save = lazy(() => import("./Page/Save"));
+const Archived = lazy(() => import("./Page/Archived"));
+const Followers = lazy(() => import("./Page/Followers"));
+const ProfileView = lazy(() => import("./Page/ProfileView"));
+const Topic = lazy(() => import("./Page/Topic"));
+
+// Catch-All Route Imports
+const NotFound = lazy(() => import("./Page/NotFound"));
 
 function App() {
+  function updateState(
+    userData: any,
+    accessToken: any,
+    following: any,
+    posts: any,
+    userNames: any
+  ) {
+    setUser((prev) => ({
+      ...prev,
+      user: { ...prev.user, ...userData },
+      isAuthenticated: true,
+      accessToken,
+    }));
+    setFollowing((prev) => ({ ...prev, following }));
+    setPostsAtom(posts);
+    setAllUsersNames(userNames);
+  }
   const [user, setUser] = useRecoilState(authAtom);
   const setFollowing = useSetRecoilState(followAtom);
   const setPostsAtom = useSetRecoilState(postsAtom);
   const setAllUsersNames = useSetRecoilState(allUsersNames);
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        // <UnProtectedRoute>
-        <Home />
-        // </UnProtectedRoute>
-      ),
-    },
-
-    {
-      path: "/register",
-      element: (
-        <UnProtectedRoute>
-          <Signup />
-        </UnProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/login",
-      element: (
-        <UnProtectedRoute>
-          <Signin />
-        </UnProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/blogs",
-      element: (
-        <ProtectedRoute>
-          <Blogs></Blogs>
-        </ProtectedRoute>
-      ),
-    },
-
-    {
-      path: "/profile/:username",
-      element: (
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "blog/:slug",
-      element: (
-        <ProtectedRoute>
-          <BlogContent />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/post/draft",
-      element: (
-        <ProtectedRoute>
-          <Draft />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/post/handle",
-      element: (
-        <ProtectedRoute>
-          <HandlePost />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/post/saved",
-      element: (
-        <ProtectedRoute>
-          <Save />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/post/archived",
-      element: (
-        <ProtectedRoute>
-          <Archived />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/user/followers",
-      element: (
-        <ProtectedRoute>
-          <Followers />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/view/profile/:username",
-      element: (
-        <ProtectedRoute>
-          <ProfileView />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/topic/:categoryName",
-      element: (
-        <ProtectedRoute>
-          <Topic />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/noveledit",
-      element: <NoveEditor />,
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/novelpreview",
-      element: <NovelPreview />,
-      errorElement: <NotFound />,
-    },
-
-    {
-      path: "/videoplayer",
-      element: <VideoPlayer />,
-    },
-
-    {
-      path: "publish",
-      element: (
-        <ProtectedRoute>
-          <Publish />
-        </ProtectedRoute>
-      ),
-      errorElement: <NotFound />,
-    },
-
-    { path: "*", element: <NotFound /> },
-  ]);
-
   const [loading, setLoading] = useState(true);
   const [hasFetched, setHasFetched] = useState(false);
 
   const resetAuthAtom = useAuthAtomResetValue();
 
+  const router = createBrowserRouter([
+    // Public Layout
+    {
+      path: "/",
+      element: <LayoutUnProtected />,
+      errorElement: <NotFound />,
+      children: [
+        { path: "home", element: SuspendedComponent(Home) },
+        { path: "register", element: SuspendedComponent(Signup) },
+        { path: "login", element: SuspendedComponent(Signin) },
+      ],
+    },
+
+    // Protected Layout
+    {
+      path: "/",
+      element: <LayoutProtected />,
+      errorElement: <NotFound />,
+      children: [
+        { path: "blogs", element: SuspendedComponent(Blogs) },
+        { path: "profile/:username", element: SuspendedComponent(Profile) },
+        { path: "blog/:slug", element: SuspendedComponent(BlogContent) },
+        { path: "post/draft", element: SuspendedComponent(Draft) },
+        { path: "post/handle", element: SuspendedComponent(HandlePost) },
+        { path: "post/saved", element: SuspendedComponent(Save) },
+        { path: "post/archived", element: SuspendedComponent(Archived) },
+        { path: "user/followers", element: SuspendedComponent(Followers) },
+        {
+          path: "view/profile/:username",
+          element: SuspendedComponent(ProfileView),
+        },
+        { path: "topic/:categoryName", element: SuspendedComponent(Topic) },
+      ],
+    },
+
+    // Catch-All Route
+    {
+      path: "*",
+      element: SuspendedComponent(NotFound),
+    },
+  ]);
+
   async function checkAuthOnAppMount() {
     if (!user.refreshToken || !user.accessToken) {
       resetAuthAtom();
-      return;
+      return; 
     }
-
-    // const {
-    //   accessToken,
-    //   user: userData,
-    //   following,
-    //   posts,
-    // } = await refreshAccessToken(user.refreshToken);
 
     const newAccessTokenPromise = refreshAccessToken(user.refreshToken);
 
@@ -231,18 +128,7 @@ function App() {
     const { accessToken, user: userData, following, posts } = newAccessToken;
 
     if (accessToken) {
-      setUser((prev) => ({
-        ...prev,
-        user: {
-          ...prev.user,
-          ...userData,
-        },
-        isAuthenticated: true,
-        accessToken: accessToken,
-      }));
-      setFollowing((prev: any) => ({ ...prev, following }));
-      setPostsAtom(posts);
-      setAllUsersNames(userNames);
+      updateState(userData, accessToken, following, posts, userNames);
       return;
     } else {
       resetAuthAtom();
