986bf001468a1e79bc7ef9581c7789cf9f4084f1
SwasthK <220981@sdmcujire.in>
2024-12-04 13:23:06 +0530

    Structure save Page

---
+++ frontend/src/Pages/Save/index.tsx
@@ -0,0 +1,58 @@
+import { lazy, Suspense } from "react";
+import { useGetAllUserSavedPosts } from "../../services/queries";
+import ErrorBar from "../../components/Error";
+import { Saved_Post_Skeleton } from "../../Skeleton/Saved_Post_Skeleton";
+import { GridIcon } from "../../assets/svg/GridIcon";
+import { RefreshIcon } from "../../assets/svg/RefreshIcon";
+
+const SaveCard = lazy(() => import("./savecard"));
+
+const Save = () => {
+  const { data, isLoading, isError, refetch, isRefetching, error } =
+    useGetAllUserSavedPosts();
+
+  return (
+    <>
+      <div className="flex justify-center items-center flex-row w-screen sm:p-4">
+        <div className="flex items-center justify-between gap-4 py-6 sm:py-4  w-[50rem] px-8 bg-[#2F2F2F] sm:rounded-lg">
+          <div className="flex items-center gap-4">
+            <h1 className="md:text-base font-semibold">Saved Posts</h1>
+            <GridIcon size={22} />
+          </div>
+          <div className="flex items-center">
+            <RefreshIcon onClick={refetch} size={20} />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-row w-screen sm:px-8">
+        <div
+          className={`${
+            isError ? "" : "grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5"
+          } w-[50rem] p-2 gap-2`}
+        >
+          {isLoading || isRefetching ? (
+            <>
+              {Array.from({ length: 15 }).map((_, index) => (
+                <Saved_Post_Skeleton key={index} />
+              ))}
+            </>
+          ) : isError ? (
+            <>
+              <ErrorBar
+                label={error.message}
+                parentClass="flex justify-center items-center"
+                childClass="bg-red-500  px-16 mt-8 text-center py-3 rounded-lg font-semibold"
+              />
+            </>
+          ) : (
+            <Suspense fallback={<Saved_Post_Skeleton />}>
+              <SaveCard data={data} />
+            </Suspense>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Save;
---
+++ frontend/src/Pages/Save/savecard.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import { Link } from "react-router-dom";
+
+const SaveCard = memo(({ data }: { data }) => {
+  return (
+    <>
+      {data?.map((post: any) => (
+        <div className="relative overflow-hidden rounded-lg group">
+          <img
+            src={post.coverImage}
+            className="aspect-square object-cover group-hover:scale-[1.2] duration-500
+              transofrm transition-all cursor-pointer ease-out"
+          ></img>
+          <div className="flex justify-center items-center absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+            <Link
+              to={`/blog/${post.slug}`}
+              className="font-semibold border px-2 py-1 text-sm bg-white text-black rounded-lg focus:bg-gray-300 focus:outline-none border-none"
+            >
+              View
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+});
+
+export default SaveCard;
