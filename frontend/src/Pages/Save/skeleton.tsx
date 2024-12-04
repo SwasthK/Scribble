da2018bb60d36d19578cfeeb89b35a452390c6b7
SwasthK <220981@sdmcujire.in>
2024-12-04 23:27:17 +0530

    Update save Page skelton

---
+++ frontend/src/Pages/Save/skeleton.tsx
@@ -0,0 +1,10 @@
+import { memo } from "react"
+
+export const Saved_Post_Skeleton = memo(() => {
+  return (
+    <div role="status" className="skeleton-parent aspect-square rounded-lg">
+        <div className="skeleton-child aspect-square rounded-lg">
+        </div>
+    </div>
+  )
+});
