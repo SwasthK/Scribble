564e9521af201c3a7065a0f2f3a6d2eb95a0f459
SwasthK <220981@sdmcujire.in>
2024-12-11 16:37:35 +0530

    Update Button Component

--- frontend/src/components/Buttons/ctaButton.tsx
+++ frontend/src/components/Buttons/ctaButton.tsx
@@ -1,39 +1,42 @@
 import { memo } from "react";
-import { useNavigate } from "react-router-dom";
 import { cn } from "../../lib/utils";
 
 export const CtaButton = memo(
   ({
     label1,
     label2,
-    url1,
-    url2,
+    onClick1,
+    onClick2,
     className1,
     className2,
+    disabled
   }: {
     label1: string;
     label2: string;
-    url1: string;
-    url2: string;
+    onClick1?: any;
+    onClick2?: any;
     className1?: string;
     className2?: string;
+    disabled?: boolean;
   }) => {
-    const navigate = useNavigate();
+ 
     return (
       <>
         <button
-          onClick={() => navigate(url1)}
+          disabled={disabled}
+          onClick={onClick1}
           className={cn(
-            `px-3 py-3.5 sm:px-10 sm:py-5 rounded-full border text-black bg-white `,
+            `text-sm sm:text-base px-3 py-2 sm:px-10 sm:py-5 rounded-full border text-black bg-white disabled:cursor-not-allowed`,
             className1
           )}
         >
           {label1}
         </button>
         <button
-          onClick={() => navigate(url2)}
+          disabled={disabled}
+          onClick={onClick2}
           className={cn(
-            "font-scribble2 px-6 py-2 sm:px-10 sm:py-5 rounded-full border text-white bg-cdark-300",
+            "text-sm sm:text-base font-scribble2 px-4 py-2 sm:px-10 sm:py-5 rounded-full border text-white bg-cdark-300 disabled:cursor-not-allowed",
             className2
           )}
         >
